3506162d7accef307689cd7ecc3d9a60d0c24071
Phuong Dang <ophda002@edu.xamk.fi>
2019-10-06 16:27:31 +0300

    Add project files.

---
+++ SDPPhuong4A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SDPPhuong4A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
